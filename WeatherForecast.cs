2e0db27e83abcdcb05d86b1fc0a6a63215a737dd
vic-gfuentes <victor.gomez2000@hotmail.com>
2021-09-22 19:48:04 -0600

    Initial Commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace servicios_web_proyecto
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
